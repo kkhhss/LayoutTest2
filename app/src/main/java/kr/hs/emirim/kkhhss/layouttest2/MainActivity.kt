99e2cd712dd4d6409d9d3a7cd7ef57c65a11352e
kkhhss <yg91238405@gmail.com>
2023-05-30 19:27:25 +0900

    코틀린 클래스에서 LinearLayout 설정

--- app/src/main/java/kr/hs/emirim/kkhhss/layouttest2/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kkhhss/layouttest2/MainActivity.kt
@@ -1,11 +1,18 @@
 package kr.hs.emirim.kkhhss.layouttest2
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val linear1 = LinearLayout(this)
+        linear1.orientation = LinearLayout.VERTICAL
+        linear1.setBackgroundColor(Color.rgb(0,255,200))
+        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        setContentView(linear1, params)
     }
 }
