ba7e8d7bd303ecc7a583fdfb96a2781eba573958
kkhhss <yg91238405@gmail.com>
2023-05-30 19:38:14 +0900

    코틀린 클래스에서 Button 생성 및 기능 설정

--- app/src/main/java/kr/hs/emirim/kkhhss/layouttest2/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kkhhss/layouttest2/MainActivity.kt
@@ -3,7 +3,9 @@ package kr.hs.emirim.kkhhss.layouttest2
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +16,14 @@ class MainActivity : AppCompatActivity() {
         linear1.setBackgroundColor(Color.rgb(0,255,200))
         val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
         setContentView(linear1, params)
+
+        val btn = Button(this)
+        btn.text = "클릭해 보세요"
+        btn.setBackgroundColor(Color.rgb(255,0,255))
+        linear1.addView(btn)
+
+        btn.setOnClickListener{
+            Toast.makeText(applicationContext, "클래스에서 생성된 버튼입니다.", Toast.LENGTH_LONG).show()
+        }
     }
 }
